ee1eeb07006b5b109efb10bee7784e30405406d0
joanLeBaptiste <105108292+joanLeBaptiste@users.noreply.github.com>
2024-03-17 12:21:55 +0100

    Create client.php / php du client

---
+++ app/public/client.php
@@ -0,0 +1,49 @@
+<?php
+require_once '../vendor/autoload.php';
+
+use App\Page;
+
+$page = new Page();
+
+$user = $page->session->get('User');
+if ($user) {
+    $user_nom = $page->session->getNom();
+
+    $user_prenom = $page->session->getPrenom();
+}
+$action = isset($_GET['action']) ? $_GET['action'] : '';
+
+$tableData = $columnNames = [];
+
+$specificINTid = $page->GetIntclient($page->session->getID(), 'clientID');
+//var_dump($specificINTid);
+var_dump($page->session->getID());
+
+switch ($action) {
+
+    case 'intervention':
+        $tableData = [
+            'intervention'=>$specificINTid,
+            'urgence'=>$page->getUrgence(),
+            'statut'=>$page->getStatut(),
+            'intervenant'=>$page->intervenantIntervention()
+        ];
+        $columnNames = ['interventionID', 'date', 'titre', 'heure', 'adresse', 'urgence', 'statut', 'intervenant'];
+       // var_dump($tableData);
+
+        break; 
+    default:
+        break;
+}
+/*
+if (!empty($tableData)) {
+    $columnNames = array_keys($tableData);
+}*/
+
+
+echo $page->render('client.html.twig', [
+    'NomPrenom' => $user_nom . " " . $user_prenom,
+    'columnNames' => $columnNames,
+    'tableData' => $tableData,
+    'action' => $action,
+]);
